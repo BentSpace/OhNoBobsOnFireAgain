c4a74074908601cfa12571eacc18781554945631
Nathan Bertram <bentspacetime@gmail.com>
2023-03-28 16:27:37 -0700

    fixed fire bug

--- Assets/Scripts/Particles.cs
+++ Assets/Scripts/Particles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,8 +8,21 @@ public class Particles : MonoBehaviour
     public GameObject particleEffect;
     public GameObject fire;
     public GameObject winMessage;
+
+    // private void Start()
+    // {
+    //     if (fire == null)
+    //     {
+    //         fire = GameObject.Find("FireBall");
+    //     }
+    // }
+
     void OnCollisionEnter(Collision coll)
     {
+        if (fire == null)
+        {
+            fire = GameObject.Find("FireBall");
+        }
         if (coll.collider.CompareTag("Player"))
         {
              particleEffect.SetActive(true);
